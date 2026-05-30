aa2a9ff7f42b58a45f06d9f75042a4410aaf4940
stuart <stacy.saunders.2002@gmail.com>
2026-05-30 07:23:53 -0400

    sync v1.0.3 release files

--- engine/src/integ_tests/search_compat.rs
+++ engine/src/integ_tests/search_compat.rs
@@ -1,4 +1,4 @@
-//! Stub summary for /Users/stuart/parallel_development/flapjack_dev/may29_pm_2_mirror_ci_green/flapjack_dev/engine/src/integ_tests/search_compat.rs.
+//! Stub summary for /Users/stuart/parallel_development/flapjack_dev/may29_pm_5_release_v1_0_3_cut/flapjack_dev/engine/src/integ_tests/search_compat.rs.
 use crate::index::SearchOptions;
 use crate::types::{FacetRequest, Filter, SearchResult, Sort};
 use crate::{error::Result, IndexManager};
@@ -33,6 +33,8 @@ fn legacy_search_options<'a>(
 /// compiling during API migration.
 ///
 /// This trait is test-only compatibility glue and is intentionally not used by production code.
+/// TODO: Document SearchCompat.
+/// TODO: Document SearchCompat.
 /// TODO: Document SearchCompat.
 /// TODO: Document SearchCompat.
 /// TODO: Document SearchCompat.
